50bf7a1464e57d671082297b26a1a3be1abb470f
RAFAEL BRUM DA FONSECA <fonseca.wutang@gmail.com>
2022-01-31 11:21:34 +0000

    atualizado painel e botoes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*var resultados = []
+
+for(var index = 0; index < numeros.length; index++) {
+  var elementoMultiplicadoPorDois = numeros[index] * 2
+  resultados.push(elementoMultiplicadoPorDois)
+}*/
+
+
